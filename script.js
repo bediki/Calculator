983fdba3dd74065aa3ac2893c9b408436a7460fe
bediki <bedi.kilic@gmail.com>
2024-02-05 09:04:02 +0100

    Basic logic

--- script.js
+++ script.js
@@ -1,46 +1,76 @@
 
 //variables
-let operator; 
+let operator;
 let firstNumber;
-let secondNumber; 
+let secondNumber;
+let displaytext='';
 
 
-function operate(operator, firstNumber, secondNumber){
+// dom elements
+let buttonsCont = document.querySelectorAll('.btn');
+let display = document.querySelectorAll('#resultDisplay');
 
-    switch (operator){
+// add eventlisteners
+buttonsCont.forEach(buttons => {
+    buttons.addEventListener('click', (e) => {
+        
+        displaytext =displaytext+ e.target.textContent;
+        console.log(displaytext);
+        updateDisplay(displaytext);
+    })
+})
+
+
+function updateDisplay(displaytext){
+    console.log("test")
+    display.innerHTML = displaytext;
+}
+
+
+function createButtons() {
+    for (let i = 0; i < 20; i++) {
+        var operators = {}
+
+    }
+}
+
+function operate(operator, firstNumber, secondNumber) {
+
+    switch (operator) {
 
         case '+':
-            console.log(add(firstNumber,secondNumber))
-            return add(firstNumber,secondNumber);
+            console.log(add(firstNumber, secondNumber))
+            return add(firstNumber, secondNumber);
             break;
         case '-':
-            return subtract(firstNumber,secondNumber);
+            return subtract(firstNumber, secondNumber);
             break;
         case '*':
-            return multiply(firstNumber,secondNumber);
+            return multiply(firstNumber, secondNumber);
             break;
         case '+/':
-            return divide(firstNumber,secondNumber);
+            return divide(firstNumber, secondNumber);
             break;
+
     }
 
 }
 
 
-function add(a, b){
-    return a+b;
+function add(a, b) {
+    return a + b;
 }
 
-function subtract(a, b){
-    return a-b;
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply(a, b){
-    return a*b;
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide(a, b){
-    return a/b;
+function divide(a, b) {
+    return a / b;
 }
 
 
